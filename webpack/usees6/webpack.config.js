9200be11a8f369d4331469a48a89a793297a4dd5
yulifu <yulifu@douyu.tv>
2016-05-19 19:49:12 +0800

    add react demo

--- webpack/usees6/webpack.config.js
+++ webpack/usees6/webpack.config.js
@@ -12,6 +12,8 @@ module.exports = {
     module: {
         loaders: [
             // babel-loader 依赖 babel-core 和 babel-preset-es2015 需要手动安装上
+            // babel-core 是 babel 的 api
+            // babel-preset-es2015 是 babel 的一个预设 用来转换 es6 代码
             // 在 npm@3 之后 npm 不会自动安装 peerDependencies
             {test: /\.js$/, loader: 'babel', query: {presets: ['es2015']}}
         ]
