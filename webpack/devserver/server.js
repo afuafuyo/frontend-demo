0fc495a0481708d2d7e59288795a2a646e40500c
yulifu <yulifu@douyu.tv>
2016-06-03 13:56:26 +0800

    add hot mocule replac to devserver

--- webpack/devserver/server.js
+++ webpack/devserver/server.js
@@ -1,11 +1,28 @@
+/**
+ * 配置修改代码自动刷新 2 中方式
+ *      1. 添加 webpack-dev-server/client?http://<path>:<port>/ 配置到所有的 entry 中
+ *      2. 在页面中引入 js 实现自动刷新 即 <script src="/webpack-dev-server.js"></script>
+ *      这里我们使用第一种方式把配置放到我们的 entry ( 这里只有一个 entry ) 中实现自动刷新
+ *
+ * Hot Module Replacement with node.js API 3 步
+ *      add an entry point to the webpack configuration: webpack/hot/dev-server
+ *      add the new webpack.HotModuleReplacementPlugin() to the webpack plugins configuration
+ *      add hot: true to the webpack-dev-server configuration to enable HMR on the server
+ *
+ *  var config = require("./webpack.config.js");  // plugins 配置中增加 new webpack.HotModuleReplacementPlugin()
+ *  config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/", "webpack/hot/dev-server");
+ *  var compiler = webpack(config);
+ *  var server = new webpackDevServer(compiler, {
+ *      hot: true
+ *      ...
+ *  });
+ *  server.listen(8080);
+ *
+ */
 var WebpackDevServer = require("webpack-dev-server");
 var webpack = require("webpack");
 
-var config = require('./webpack.config');
-// 配置修改代码自动刷新
-//      1. 添加 webpack-dev-server/client?http://<path>:<port>/ 配置到所有的 entry 中
-//      2. 在页面中引入 js 实现自动刷新 即 <script src="/webpack-dev-server.js"></script>
-//      这里我们使用第一种方式把配置放到我们的 entry ( 这里只有一个 entry ) 中实现自动刷新
+var config = require('./webpack.config');     
 config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
 var compiler = webpack(config);
 
