10373a238adc9bfd04c38d8039641b2bbd60302b
yulifu <yulifu@douyu.tv>
2017-11-21 14:12:40 +0800

    change js

--- react/basicreactapp/app.js
+++ react/basicreactapp/app.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import App from './containers/App';
 import A from './containers/App/A.js';
 import B from './containers/App/B.js';
 
 render(
-    <Router history={hashHistory}>
-        <Route path='/' component={App}>
-            <Route path='a' component={A} />
-            <Route path='b' component={B} />
-        </Route>
-    </Router>,
+    <HashRouter>
+        <Switch>
+            <Route exact path='/' component={App}/>
+            <Route path='/a' component={A}/>
+            <Route path='/b' component={B}/>
+        </Switch>
+    </HashRouter>,
     document.getElementById('app')
 );
--- react/basicreactapp/containers/App/index.js
+++ react/basicreactapp/containers/App/index.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
     render() {
         return (
         <div>
-        {this.props.children}
+            this is home page
         </div>
         )
     }
