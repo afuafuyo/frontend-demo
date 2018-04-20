53efcf3f735befffc4e6ec0b932ab9aa9bc81711
yulifu <yulifu@douyu.tv>
2018-04-20 18:17:33 +0800

    change js

---
+++ js/Display/src/DisplayObject.js
@@ -0,0 +1,52 @@
+import HashObject from './HashObject';
+
+/**
+ * The DisplayObject class is the base class for all objects that can be display on screen
+ */
+export default class DisplayObject extends HashObject {
+
+    constructor() {
+        super();
+
+        /**
+         * document 对象引用
+         */
+        this.doc = document;
+
+        /**
+         * 文档片段
+         */
+        this.fragement = this.doc.createDocumentFragment();
+
+        /**
+         * 对象的 dom 对象形式
+         */
+        this.dom = null;
+    }
+
+    /**
+     * 初始化事件
+     */
+    bindEvent() {}
+
+    /**
+     * 获取 html 文档片段 这个文档片段只能有一个父级元素
+     */
+    render() {
+        return '';
+    }
+
+    /**
+     * 获取 dom 对象
+     */
+    getDom() {
+        let html = this.render();
+        let tmp = this.doc.createElement('div');
+
+        tmp.innerHTML = html;
+        this.fragement.appendChild(tmp);
+
+        return this.dom = this.fragement.firstChild.firstChild;
+    }
+
+}
